2f04330c07198c3261580b08afbd84f8b79282fb
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-03-03 17:28:02 +0100

    Update powermenu.sh

--- powermenu.sh
+++ powermenu.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-yad --button yad-close --button='Reboot!amarok_playlist_refresh:3' --button yad-ok --fixed -center --title "Powermenu" --text "Shutdown will automatically commence in 60 seconds"  --timeout 60 --buttons-layout=center --timeout-indicator bottom
+yad --button yad-close  --button='Lock!lock:5'  --button='Logout!document-export:6' --button='Reboot!amarok_playlist_refresh:3' --button yad-ok --fixed -center --title "Powermenu" --text "Shutdown will automatically commence in 60 seconds"  --timeout 60 --buttons-layout=center --timeout-indicator bottom
 
 case $? in
-	2)poweroff;;
+	4)poweroff;;
 	70)poweroff;;
 	3)reboot;;
+	6)swaymsg exit;;
+	5)/home/all/Scripts/lock.sh || pkill -KILL -u $USER;;
 esac
